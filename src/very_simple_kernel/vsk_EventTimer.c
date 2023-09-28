1e76de923770d559fceada06165fcb174ed63f14
Alex Marincu <alex.marincu@gmail.com>
2023-09-28 22:19:30 +0300

    Added critical sections to task and event timer

--- src/very_simple_kernel/vsk_EventTimer.c
+++ src/very_simple_kernel/vsk_EventTimer.c
@@ -1,4 +1,5 @@
 #include "vsk_EventTimer.h"
+#include "vsk_CriticalSection.h"
 
 struct vsk_EventTimerClass {
     vsk_Time * sysTime;
@@ -39,22 +40,29 @@ vsk_EventTimer * vsk_EventTimer_init(vsk_EventTimer * const self, vsk_Event * co
 }
 
 void vsk_EventTimer_arm(vsk_EventTimer * const self, uint32_t const delayMillis, uint32_t const periodMillis) {
+    vsk_CriticalSection_enter(vsk_CriticalSection_());
     self->delayMillis = delayMillis;
     self->periodMillis = periodMillis;
+    vsk_CriticalSection_exit(vsk_CriticalSection_());
 }
 
 void vsk_EventTimer_disarm(vsk_EventTimer * const self) {
+    vsk_CriticalSection_enter(vsk_CriticalSection_());
     self->delayMillis = 0;
     self->periodMillis = 0;
+    vsk_CriticalSection_exit(vsk_CriticalSection_());
 }
 
 static void vsk_EventTimer_onSysTick(vsk_EventTimer * const self) {
+    vsk_CriticalSection_enter(vsk_CriticalSection_());
     if (self->delayMillis != 0) {
         if (self->delayMillis <= vsk_Time_getTickPeriodMillis(vsk_Time_())) {
             self->delayMillis = self->periodMillis;
+            vsk_CriticalSection_exit(vsk_CriticalSection_());
             vsk_Event_raise(self->event);
         } else {
             self->delayMillis -= vsk_Time_getTickPeriodMillis(vsk_Time_());
+            vsk_CriticalSection_exit(vsk_CriticalSection_());
         }
     }
 }
--- src/very_simple_kernel/vsk_Task.c
+++ src/very_simple_kernel/vsk_Task.c
@@ -1,4 +1,5 @@
 #include "vsk_Task.h"
+#include "vsk_CriticalSection.h"
 #include "vsk_Time.h"
 
 vsk_Task * vsk_Task_init(
@@ -10,14 +11,21 @@ vsk_Task * vsk_Task_init(
 }
 
 bool vsk_Task_isReady(vsk_Task * const self) {
-    return (ut_Queue_isEmpty(self->messageQueue) == false);
+    vsk_CriticalSection_enter(vsk_CriticalSection_());
+    bool isReady = (ut_Queue_isEmpty(self->messageQueue) == false);
+    vsk_CriticalSection_exit(vsk_CriticalSection_());
+    return isReady;
 }
 
 void vsk_Task_run(vsk_Task * const self) {
+    vsk_CriticalSection_enter(vsk_CriticalSection_());
     vsk_TaskMessage * message = ut_Queue_dequeue(self->messageQueue);
+    vsk_CriticalSection_exit(vsk_CriticalSection_());
     message->handler(self);
 }
 
 void vsk_Task_postMessage(vsk_Task * const self, vsk_TaskMessage * const message) {
+    vsk_CriticalSection_enter(vsk_CriticalSection_());
     ut_Queue_enqueue(self->messageQueue, message);
+    vsk_CriticalSection_exit(vsk_CriticalSection_());
 }
