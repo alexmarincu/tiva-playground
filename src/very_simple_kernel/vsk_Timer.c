07e1bc062e5df494480a66c4eaf81b6352a09655
Alex Marincu <alex.marincu@gmail.com>
2023-10-19 23:13:40 +0300

    Updated timer

--- src/very_simple_kernel/vsk_Timer.c
+++ src/very_simple_kernel/vsk_Timer.c
@@ -8,6 +8,7 @@ vsk_Timer * vsk_Timer_init(
     vsk_Timer * const self,
     vsk_TimerCallback const callback
 ) {
+    vsk_Assert_true(vsk_Assert_(), callback != NULL);
     self->_callback = callback;
     self->_delayMillis = 0;
     self->_periodMillis = 0;
@@ -21,25 +22,20 @@ void vsk_Timer_arm(
     uint32_t const periodMillis
 ) {
     vsk_Assert_true(vsk_Assert_(), delayMillis > 0);
-    vsk_CriticalSection_enter(vsk_CriticalSection_());
     self->_delayMillis = delayMillis;
     self->_periodMillis = periodMillis;
-    vsk_CriticalSection_exit(vsk_CriticalSection_());
 }
 /*............................................................................*/
 void vsk_Timer_disarm(
     vsk_Timer * const self
 ) {
-    vsk_CriticalSection_enter(vsk_CriticalSection_());
     self->_delayMillis = 0;
     self->_periodMillis = 0;
-    vsk_CriticalSection_exit(vsk_CriticalSection_());
 }
 /*............................................................................*/
 void vsk_Timer_onSysTick(
     vsk_Timer * const self
 ) {
-    vsk_CriticalSection_enter(vsk_CriticalSection_());
     if (self->_delayMillis != 0) {
         if (self->_delayMillis <= vsk_Time_getTickPeriodMillis(vsk_Time_())) {
             self->_delayMillis = self->_periodMillis;
@@ -48,5 +44,4 @@ void vsk_Timer_onSysTick(
             self->_delayMillis -= vsk_Time_getTickPeriodMillis(vsk_Time_());
         }
     }
-    vsk_CriticalSection_exit(vsk_CriticalSection_());
 }
